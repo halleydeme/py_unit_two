419baf8d055b3184cafa89628e808de270a9f9bb
halley <demeha@ssfs.org>
2021-09-20 11:25:12 -0400

    started unit 2 assignment

---
+++ assignment_two.py
@@ -0,0 +1,7 @@
+name = input("what is your name")
+fn = input("what is your favorite number?")
+dc = input("what is your dream car")
+dcc = input("what is the cost of your dream car?")
+loan = input( "If you were to take out a loan, how many years would it be for?")
+
+print( "Hi, I'm Halley!", name, "That is a beautiful name.", fn , "Awesome! Mine is 21." , dc , "That's mine too!" , int(dcc), int(loan), )
